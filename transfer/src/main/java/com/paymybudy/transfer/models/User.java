8d7a730db45ddda946958e57c3735040bc5c67f7
jedi10 <kyle10_dev@hotmail.fr>
2020-10-03 21:15:24 +0200

    Add User Model; Unit Test

---
+++ transfer/src/main/java/com/paymybudy/transfer/models/User.java
@@ -0,0 +1,77 @@
+package com.paymybudy.transfer.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class User {
+
+    @Getter
+    @Setter
+    private String firstName;
+
+    @Getter
+    @Setter
+    private String lastName;
+
+    @Getter
+    @Setter
+    private String email;
+
+    @Getter
+    @Setter
+    private String password;
+
+    @Getter
+    private List<User> contactList;
+
+    @Getter
+    private List<?> externalTransactionList;
+
+    @Getter
+    private List<?> internalTransactionList;
+
+    /**
+     * <b>User Constructor: all list will be created as empty</b>
+     * @param firstName firstName
+     * @param lastName lastName
+     * @param email email
+     * @param password password
+     */
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        setContactList(null);
+        setExternalTransactionList(null);
+        setInternalTransactionList(null);
+    }
+
+    public void setContactList(List<User> userList){
+        this.contactList = Optional.ofNullable(userList)
+                .map(List::stream)
+                .orElseGet(Stream::empty)
+                .collect(Collectors.toList());
+    }
+
+    public void setExternalTransactionList(List<?> externalList){
+        this.externalTransactionList = Optional.ofNullable(externalList)
+                .map(List::stream)
+                .orElseGet(Stream::empty)
+                .collect(Collectors.toList());
+    }
+
+    public void setInternalTransactionList(List<?> internalList){
+        this.internalTransactionList = Optional.ofNullable(internalList)
+                .map(List::stream)
+                .orElseGet(Stream::empty)
+                .collect(Collectors.toList());
+    }
+
+
+}
