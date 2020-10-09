9a55ee6cdfce26375fe03c966a0496cb2d65cefc
jedi10 <kyle10_dev@hotmail.fr>
2020-10-09 11:09:36 +0200

    DAL Package Creation; JPA Config activation and optimization
    Trying to use a schema.sql file but with no success for the moment. So it's useless for now.

---
+++ transfer/src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+/--DROP DATABASE IF EXISTS transfer;
+/--CREATE DATABASE transfer CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `transfer` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `transfer`;
+
+
+
+/--https://dev.mysql.com/doc/refman/8.0/en/charset-database.html
