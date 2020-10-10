73b19ee9e0fdd44881fca728a7d61f847867e8b4
jedi10 <kyle10_dev@hotmail.fr>
2020-10-10 17:52:50 +0200

    Add H2 Database for Test
    H2 Database has been added for testing. Test have their own database and need no more external resource.

--- transfer/src/main/resources/schema.sql
+++ transfer/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 /--DROP DATABASE IF EXISTS transfer;
 /--CREATE DATABASE transfer CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE DATABASE IF NOT EXISTS `transfer` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `transfer`;
+/--CREATE DATABASE IF NOT EXISTS `transfer` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+/--USE `transfer`;
 
 
 
