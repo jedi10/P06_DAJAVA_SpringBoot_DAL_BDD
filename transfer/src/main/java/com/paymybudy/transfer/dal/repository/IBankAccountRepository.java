3f0a9a27835a3e4944f254fc722ca662e6d3ad69
jedi10 <kyle10_dev@hotmail.fr>
2020-10-15 18:22:31 +0200

    Update BankAccount Repository

--- transfer/src/main/java/com/paymybudy/transfer/dal/repository/IBankAccountRepository.java
+++ transfer/src/main/java/com/paymybudy/transfer/dal/repository/IBankAccountRepository.java
@@ -2,8 +2,9 @@ package com.paymybudy.transfer.dal.repository;
 
 import com.paymybudy.transfer.models.BankAccount;
 import com.paymybudy.transfer.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface IBankAccountRepository extends CrudRepository<BankAccount, Long> {
-    
+public interface IBankAccountRepository extends JpaRepository<BankAccount, Long> {
+
 }
