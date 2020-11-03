d86aa6f498b2cbef25007145c784618377c7162f
jedi10 <kyle10_dev@hotmail.fr>
2020-11-03 18:33:01 +0100

    Add Dal Service for moneyTransferType; Unit Test Ok

--- transfer/src/main/java/com/paymybudy/transfer/dal/repository/IMoneyTransferTypeRepository.java
+++ transfer/src/main/java/com/paymybudy/transfer/dal/repository/IMoneyTransferTypeRepository.java
@@ -1,8 +1,9 @@
 package com.paymybudy.transfer.dal.repository;
 
 import com.paymybudy.transfer.models.MoneyTransferType;
+import com.paymybudy.transfer.models.MoneyTransferTypeKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IMoneyTransferTypeRepository extends JpaRepository<MoneyTransferType, Long> {
+public interface IMoneyTransferTypeRepository extends JpaRepository<MoneyTransferType, MoneyTransferTypeKey> {
 
 }
